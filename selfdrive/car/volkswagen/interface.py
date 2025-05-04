a74a69991bc6ad2753a0c866172457e65fc1b3a7
Dennis <43215753+Dennis-NL@users.noreply.github.com>
2025-05-04 03:50:52 +0200

    Torque increase

--- selfdrive/car/volkswagen/interface.py
+++ selfdrive/car/volkswagen/interface.py
@@ -82,9 +82,13 @@ class CarInterface(CarInterfaceBase):
 
     # Global lateral tuning defaults, can be overridden per-vehicle
 
-    ret.steerLimitTimer = 0.4
+    ret.steerLimitTimer = 1.0
     if ret.flags & VolkswagenFlags.PQ or ret.flags & VolkswagenFlags.MLB:
-      ret.steerActuatorDelay = 0.2
+      ret.steerActuatorDelay = 0.13
+      ret.steerRatio = 16.0
+      ret.mass = 1979.
+      ret.wheelbase = 2.8
+      ret.centerToFront = ret.wheelbase * 0.46
       CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
     else:
       ret.steerActuatorDelay = 0.1
