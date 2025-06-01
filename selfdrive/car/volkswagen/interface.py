a19fbcf6b9b2e44a8d04a33f07964d24c6c69c3b
actuallylemoncurd <curtis.x.jenkins@gmail.com>
2025-06-01 16:38:11 -0400

    vw pq; add a little PI

--- selfdrive/car/volkswagen/interface.py
+++ selfdrive/car/volkswagen/interface.py
@@ -115,6 +115,10 @@ class CarInterface(CarInterfaceBase):
       ret.longitudinalTuning.kiV =  [.69]
       ret.longitudinalActuatorDelay = 0.6
       ret.steerControlType = car.CarParams.SteerControlType.angle
+      ret.lateralTuning.pid.kpBP = [0., 20.]
+      ret.lateralTuning.pid.kiBP = [0., 20.]
+      ret.lateralTuning.pid.kpV = [0.01, 0.]
+      ret.lateralTuning.pid.kiV = [0.001, 0.001]
     else:
       ret.steerActuatorDelay = 0.1
       ret.lateralTuning.pid.kpBP = [0.]
