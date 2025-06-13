73096aa426e954c680384eca42d25d38a1560681
Dennis <43215753+Dennis-NL@users.noreply.github.com>
2025-06-13 17:43:17 +0200

    Update mlbcan.py

--- selfdrive/car/volkswagen/mlbcan.py
+++ selfdrive/car/volkswagen/mlbcan.py
@@ -2,7 +2,7 @@ def create_steering_control(packer, bus, apply_angle, PLA_status, PLA_ESP_status
   values = {
     "PLA_Status_PLA_EPS": PLA_status,
     "PLA_LW_Soll": abs(apply_angle),
-    "PLA_VZ_LW_Soll": (1 if apply_angle < 0 else 0) if PLA_status == 6,
+    "PLA_VZ_LW_Soll": (1 if apply_angle < 0 else 0) if PLA_status == 6 else 0,
     "PLA_Status_PLA_ESP": PLA_ESP_status,
     "PLA_Bremsmoment": 0,
     "PLA_Bremsverzoegerung": 0,
