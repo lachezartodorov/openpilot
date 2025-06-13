bc7c25d703c001d72bfdd01a71bf29eead130724
Dennis <43215753+Dennis-NL@users.noreply.github.com>
2025-06-13 18:02:09 +0200

    Update carcontroller.py

--- selfdrive/car/volkswagen/carcontroller.py
+++ selfdrive/car/volkswagen/carcontroller.py
@@ -138,7 +138,7 @@ class CarController(CarControllerBase):
     if status in (1, 6, 10):
       self.CCP.STEER_STEP = 2    # 50Hz
     elif status == 8:
-      self.CCP.STEER_STEP = 10  # 10Hz
+      self.CCP.STEER_STEP = 2  # 50Hz
     else:
       self.CCP.STEER_STEP = 2   # 50 hz
 
