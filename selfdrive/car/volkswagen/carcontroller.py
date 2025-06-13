6054bcaab5d8748348c68b44d0761100db9a07ab
Teal <teal@teallvbs.xyz>
2025-06-13 12:52:21 -0500

    VW MQB: PLA bugfix 2

--- selfdrive/car/volkswagen/carcontroller.py
+++ selfdrive/car/volkswagen/carcontroller.py
@@ -192,6 +192,7 @@ class CarController(CarControllerBase):
     self.apply_angle_last = apply_angle
     self.CSsteeringAngleDegLast = CS.out.steeringAngleDeg
     can_sends.append(self.CCS.create_steering_control(self.packer_pt, CANBUS.br, apply_angle, self.PLA_Status, self.PLA_ESP_Status))
+    apply_steer = 0
     can_sends.append(self.CCS.HCA(self.packer_pt, CANBUS.pt, False, False))
 
     if self.CP.flags & VolkswagenFlags.STOCK_HCA_PRESENT:
