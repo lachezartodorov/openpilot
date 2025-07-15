a031f1f0193dc86b8e368bfe2f96ee21d0b794f1
Dennis <43215753+Dennis-NL@users.noreply.github.com>
2025-07-15 23:11:53 +0200

    Testing for up!

--- selfdrive/car/volkswagen/carstate.py
+++ selfdrive/car/volkswagen/carstate.py
@@ -247,16 +247,16 @@ class CarState(CarStateBase):
     # Update ACC radar status.
     self.acc_type = ext_cp.vl["ACC_System"]["ACS_Typ_ACC"]
     #ret.cruiseState.available = bool(pt_cp.vl["Motor_5"]["GRA_Hauptschalter"])
-    ret.cruiseState.enabled = pt_cp.vl["Motor_2"]["GRA_Status"] in (1, 2)
-    ret.cruiseState.available = ret.cruiseState.enabled  # TEST hack for UP!f
-    if self.CP.pcmCruise:
-      ret.accFaulted = ext_cp.vl["ACC_GRA_Anzeige"]["ACA_StaACC"] in (6, 7)
-    else:
-      ret.accFaulted = pt_cp.vl["Motor_2"]["GRA_Status"] == 3
+    ret.cruiseState.enabled = True if pt_cp.vl["Motor_2"]['GRA_Status'] in [1, 2] else False
+    ret.cruiseState.available = ret.cruiseState.enabled  # TEST hack for UP!
+    #if self.CP.pcmCruise:
+    #  ret.accFaulted = ext_cp.vl["ACC_GRA_Anzeige"]["ACA_StaACC"] in (6, 7)
+    #else:
+    #  ret.accFaulted = pt_cp.vl["Motor_2"]["GRA_Status"] == 3
 
     # Update ACC setpoint. When the setpoint reads as 255, the driver has not
     # yet established an ACC setpoint, so treat it as zero.
-     # FIXME: for now this is the CC setpoint, the radar setpoint is in ACC_GRA_Anziege.ACA_V_Wunsch
+    # FIXME: for now this is the CC setpoint, the radar setpoint is in ACC_GRA_Anziege.ACA_V_Wunsch
     ret.cruiseState.speed = pt_cp.vl["Motor_2"]['Soll_Geschwindigkeit_bei_GRA_Be'] * CV.KPH_TO_MS
     if ret.cruiseState.speed > 70:  # 255 kph in m/s == no current setpoint
       ret.cruiseState.speed = 0
@@ -415,9 +415,9 @@ class MqbExtraSignals:
 class PqExtraSignals:
   # Additional signal and message lists for optional or bus-portable controllers
   fwd_radar_messages = [
-    ("ACC_System", 50),                          # From J428 ACC radar control module
-    ("ACC_GRA_Anzeige", 25),                     # From J428 ACC radar control module
+    #("ACC_System", 50),                          # From J428 ACC radar control module
+    #("ACC_GRA_Anzeige", 25),                     # From J428 ACC radar control module
   ]
   bsm_radar_messages = [
-    ("SWA_1", 20),                               # From J1086 Lane Change Assist
+    #("SWA_1", 20),                               # From J1086 Lane Change Assist
   ]
